9f0b545654a4c051671217952743b44e3edd29fa
ssardo <sergio.sardo@gmail.com>
2021-01-06 09:59:26 +0000

    Update downloadNpmDependencies.js
    Changed q and npmModule to global

--- scripts/downloadNpmDependencies.js
+++ scripts/downloadNpmDependencies.js
@@ -5,6 +5,8 @@
   var path = require('path');
   var spawnSync = require('child_process').spawnSync;
   var INSTALLFLAGNAME = '.installed';
+  var q = require('q');
+  var npmModule = require('npm');
 
   // entry
   module.exports = {
@@ -14,8 +16,8 @@
   // install the node dependencies for this project
   function install (context) {
     // set properties
-    var q = context.require('q');
-    var async = new q.defer(); // eslint-disable-line
+    var Q = context.q;
+    var async = new Q.defer(); // eslint-disable-line
     var installFlagLocation = path.join(context.opts.projectRoot, 'plugins', context.opts.plugin.id, INSTALLFLAGNAME);
     var dependencies = require(path.join(context.opts.projectRoot, 'plugins', context.opts.plugin.id, 'package.json')).dependencies;
 
